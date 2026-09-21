ae11914416d1b6a6593146cb6088c0e9d506c8a1
dreed-7896 <103713147+dreed-7896@users.noreply.github.com>
2026-09-21 21:33:22 +0600

    Add Copy URL to paged reader

--- Midoku/Features/Reader/Readers/Paged/ReaderPagedViewController.swift
+++ Midoku/Features/Reader/Readers/Paged/ReaderPagedViewController.swift
@@ -1314,7 +1314,11 @@ extension ReaderPagedViewController: UIContextMenuInteractionDelegate {
             var actions = [shareAction, saveToPhotosAction, reloadAction]
             if let image = pageView.image,
                let page = self.pageViewControllers.first(where: { $0.pageView?.imageView == pageView })?.page {
-                actions += self.delegate?.collectionCoverActions(image: image, chapterKey: page.chapterId) ?? []
+                actions += self.delegate?.collectionCoverActions(
+                    image: image,
+                    chapterKey: page.chapterId,
+                    imageURL: page.imageURL
+                ) ?? []
             }
 
             if self.usesDoublePages {
